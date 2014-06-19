1f67b4e0ec99f0f598aacd5b00e200aea082202e
bgassler <bgassler4@gmail.com>
2014-06-19 16:35:53 -0400

    lab2

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,18 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Brendan Gassler
+
+def is_triangle(a, b, c):
+	if a > b + c:
+		print "No"
+	elif b > a + c:
+		print "No"
+	elif c > a + b:
+		print "No"
+	else:
+		print "Yes"
+
+a  = raw_input('What length is the first leg?\n')
+b  = raw_input('What length is the second leg?\n')
+c  = raw_input('What length is the third leg?\n')
+
+is_triangle(a, b, c)
